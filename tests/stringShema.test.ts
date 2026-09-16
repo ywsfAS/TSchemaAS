51dc9d8670c675a6445fb52e242615e55f60f9d9
ywsfAS <akhroub.youssf@gmail.com>
2026-09-16 23:08:13 +0100

    test: add Vitest and initial string schema tests

---
+++ tests/stringShema.test.ts
@@ -0,0 +1,48 @@
+import {it,describe,expect} from "vitest";
+import {s} from "../src/schema.js";
+
+describe("string-schema",() => {
+
+    describe("parse", () => {
+
+      it("returns the value when given a string",() => {
+
+          const value = "youssefAS";
+          expect(s.string().parse(value)).toBe(value);
+
+      });
+      it("throw when given a number",() => {
+
+          const value = 10;
+          expect(() => {s.string().parse(value)}).toThrow();
+
+      });
+    })
+    describe("tryParse" , () => {
+
+      it("returns a successful result when given a string",() => {
+
+          const value = "foo";
+          expect(s.string().tryParse(value)).toEqual({
+              success : true,
+              data : value
+          });
+
+      });
+      it("returns a failure result when given a number",() => {
+
+          const value = 10;
+          expect(s.string().tryParse(value).success).toBe(false);
+      });
+    })
+
+
+
+
+
+
+
+
+
+
+})
