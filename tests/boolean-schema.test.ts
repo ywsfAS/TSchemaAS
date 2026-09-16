5eb5b8cc00012bfa117361f2484ead23bc76fdf2
ywsfAS <akhroub.youssf@gmail.com>
2026-09-16 23:16:33 +0100

    test: add number and boolean schema tests

---
+++ tests/boolean-schema.test.ts
@@ -0,0 +1,44 @@
+import { it, describe, expect } from "vitest";
+import { s } from "../src/schema.js";
+
+describe("boolean-schema", () => {
+
+    describe("parse", () => {
+
+        it("returns the value when given a boolean", () => {
+
+            const value = true;
+            expect(s.boolean().parse(value)).toBe(value);
+
+        });
+
+        it("throws when given a non-boolean", () => {
+
+            const value = "true";
+            expect(() => {
+                s.boolean().parse(value);
+            }).toThrow();
+
+        });
+    });
+
+    describe("tryParse", () => {
+
+        it("returns a successful result when given a boolean", () => {
+
+            const value = false;
+            expect(s.boolean().tryParse(value)).toEqual({
+                success: true,
+                data: value
+            });
+
+        });
+
+        it("returns a failure result when given a non-boolean", () => {
+
+            const value = 10;
+            expect(s.boolean().tryParse(value).success).toBe(false);
+
+        });
+    });
+});
---
+++ tests/number-schema.test.ts
@@ -0,0 +1,44 @@
+import { it, describe, expect } from "vitest";
+import { s } from "../src/schema.js";
+
+describe("number-schema", () => {
+
+    describe("parse", () => {
+
+        it("returns the value when given a number", () => {
+
+            const value = 10;
+            expect(s.number().parse(value)).toBe(value);
+
+        });
+
+        it("throws when given a non-number", () => {
+
+            const value = "youssefAS";
+            expect(() => {
+                s.number().parse(value);
+            }).toThrow();
+
+        });
+    });
+
+    describe("tryParse", () => {
+
+        it("returns a successful result when given a number", () => {
+
+            const value = 10;
+            expect(s.number().tryParse(value)).toEqual({
+                success: true,
+                data: value
+            });
+
+        });
+
+        it("returns a failure result when given a non-number", () => {
+
+            const value = "foo";
+            expect(s.number().tryParse(value).success).toBe(false);
+
+        });
+    });
+});
